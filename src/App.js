6d9e31ac90886500374747b3c6f88cc32bd3f4b9
Harshith Latchupatula <harshithl1777@gmail.com>
2021-03-07 09:10:11 -0500

    fixed user auth redirect issue

--- src/App.js
+++ src/App.js
@@ -3,24 +3,34 @@ import axios from 'axios';
 
 class App extends Component {
 
-
   authenticateUser = () => {
     this.API_KEY = process.env.REACT_APP_API_KEY;
-    window.open(`https://trello.com/1/authorize?response_type=token&key=${this.API_KEY}&scope=read&expiration=never&name=Roadrunner`);
-}
+    window.Trello.setKey(this.API_KEY);
+    window.Trello.authorize({
+      type: 'popup',
+      name: 'Getting Started Application',
+      scope: {
+        read: 'true',
+        write: 'true' },
+      expiration: 'never',
+      success: this.authSuccess,
+      error: this.authFailure
+    });
+  }
 
-test = () => {
+authSuccess = () => {
+  this.userToken = window.Trello.token();
   axios.get('https://api.trello.com/1/boards/9khsMGic/lists', {
     params: {
         key: this.API_KEY,
-        token: this.token.value,
+        token: this.userToken,
     }
   })
   .then(({ data }) => {
     axios.get(`https://api.trello.com/1/lists/${data[0].id}/cards?`, {
       params: {
         key: this.API_KEY,
-        token: this.token.value,
+        token: this.userToken,
       }
     })
     .then(({ data }) => {
@@ -31,12 +41,15 @@ test = () => {
   });
 }
 
+authFailure = () => {
+  console.log('Failed');
+}
+
+
   render() {
     return (
       <div className="App">
         <button onClick={this.authenticateUser}>Authenticate User</button>
-        <input ref={ref => this.token = ref} />
-        <button onClick={this.test}>Test</button>
       </div>
     );
   }
