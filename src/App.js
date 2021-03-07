e90d8190cb1369075632bc8b3362e9ee4fe05f9a
Harshith Latchupatula <harshithl1777@gmail.com>
2021-03-06 20:23:03 -0500

    created react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  authSuccess = () => {
+    console.log('Successful authentication');
+  };
+  
+  authenticationFailure = () => {
+    console.log('Failed authentication');
+  };
+
+  authenticateUser = () => {
+    window.Trello.authorize({
+      type: 'popup',
+      name: 'Roadrunner',
+      scope: { read: 'true', write: 'true' },
+      expiration: 'never',
+      success: this.authSuccess,
+      error: this.authFailure
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.authenticateUser}>Authenticate User</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
