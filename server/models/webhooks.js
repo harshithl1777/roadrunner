581e6508f1c4c7d7212b7f46d7b9f3c904730676
Harshith Latchupatula <harshithl1777@gmail.com>
2021-04-29 23:05:55 -0400

    ssl fix

--- server/models/webhooks.js
+++ server/models/webhooks.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = (process.env.NODE_ENV === 'production') 
-    ? new Pool({ connectionString: process.env.DATABASE_URL })
+    ? new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } })
     : new Pool({ host: 'localhost', user: 'postgres', database: 'roadrunner', password: process.env.LOCAL_PG_PW, port: 5432 });
 
 const createWebhook = async (data, id) => {
