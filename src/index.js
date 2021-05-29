11c16b68a7ed426362608c1884a349624fc984c3
Harshith Latchupatula <harshithl1777@gmail.com>
2021-05-28 22:36:28 -0400

    added sentry to react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from './app/App';
+
+Sentry.init({
+    dsn: process.env.REACT_APP_STATUS === 'production' ? process.env.REACT_APP_SENTRY_DSN : null,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 0.5,
+  });
 
 ReactDOM.render(<App />, document.querySelector('#root'));
