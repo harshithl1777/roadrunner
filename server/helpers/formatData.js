51122d5d9abb8a885214d20b7801d75ce1a44e49
Harshith Latchupatula <harshithl1777@gmail.com>
2021-09-26 15:01:24 -0400

    updated formatting script

--- server/helpers/formatData.js
+++ server/helpers/formatData.js
@@ -1,5 +1,6 @@
 const capitalize = (str) => {
-    return str[0].toUpperCase() + str.slice(1);
+    if (str) return str[0].toUpperCase() + str.slice(1);
+    return '';
 }
 
 const removeUnwanted = (data) => {
